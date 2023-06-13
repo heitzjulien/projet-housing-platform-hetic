988fd1897c7cb6c68bda577ef8ce379edf216836
mgfahem <mgfahem@gmail.com>
2023-06-13 16:59:12 +0200

    Add footer and styles

---
+++ src/view/template/footer.php
@@ -0,0 +1,8 @@
+<footer>
+    <div class="footer-top">
+        <span>Confidentialité</span><span>Termes d'utilisations</span>
+    </div>
+    <div class="footer-bottom">
+        <span>© 2023 . </span><span>Tous droits réservés .</span>
+    </div>
+</footer>
