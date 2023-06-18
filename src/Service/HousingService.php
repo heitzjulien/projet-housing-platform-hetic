4c43683c18dd1842350224c3d91d717edace4a43
Julien Heitz <contact@heitzjulien.com>
2023-06-18 14:49:50 +0200

    feat: search

--- src/Service/HousingService.php
+++ src/Service/HousingService.php
@@ -11,23 +11,15 @@ class HousingService {
         $this->housingRepository = new HousingRepository();
     }
 
-    public function selectHousingHome() {
-        return $this->housingRepository->selectHousingForHome();
-    }
-
-    // public function selectHousingImage() {
-    //     return $this->housingRepository->selectHousingImage();
-    // }
-
-    public function selectHousingRandomImage() {
-        return $this->housingRepository->selectHousingRandomImage();
-    }
-
     public function selectHousing($id) {
         return $this->housingRepository->selectHousing($id);
     }
 
     public function selectImageById($housing_id) {
         return $this->housingRepository->selectImageById($housing_id);
+    }
+
+    public function selectHousingForSearch(?string $unavailability_start, ?string $unavailability_end, ?string $district, ?int $nbr_rooms, ?int $capacity) {
+        return $this->housingRepository->selectHousingForSearch($unavailability_start, $unavailability_end, $district, $nbr_rooms, $capacity);
     }
 }
--- src/view/content/search.php
+++ src/view/content/search.php
@@ -1,11 +1,10 @@
 <h2>Search</h2>
 
-<?php foreach($data['start'] as $h => $c): ?>
-    <p><?= $c->getName() ?></p>
-    <p><?= $c->getCapacity() ?> personnes</p>
-    <p><?= $c->getPrice() ?> €</p>
-    <p><?= $c->getDescription() ?></p>
-    <p><?= $c->getNbr_pieces() ?> pièces</p>
-    <p><?= $c->getArea() ?> m²</p>
-    <img src="<?= $c->getImages()[0]->getImage() ?>" alt="">
-<?php endforeach; ?>
+<form method="post">
+    <input type="date" name="date_start" id="date_start">
+    <input type="date" name="date_end" id="date_end">
+    <input type="string" name="district" id="district" placeholder="district">
+    <input type="int" name="number_pieces" id="number_pieces" placeholder="nbr piece">
+    <input type="int" name="capacity" id="capacity" placeholder="capacity">
+    <input type="submit" value="submit">
+</form>
