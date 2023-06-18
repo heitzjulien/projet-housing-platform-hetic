8926ff4b28a3c2166291569fbd694a12cebd8940
Louisan TCHITOULA <t.louisan@outlook.fr>
2023-06-18 10:30:18 +0200

    feat: header & responsive & global spacing

--- src/view/template/header.php
+++ src/view/template/header.php
@@ -1,8 +1,24 @@
-<?= "Je suis le header :) " ?>
-<?php if($data['user_logged_in']): ?>
-    <?= "Hello " . ($data['user_logged_in'])->getFirstname() ?>
-    <a href="<?= __ROOT_URL__ ?>/logout">Logout</a>
-<?php else: ?>
-    <a href="<?= __ROOT_URL__ ?>/login">Login</a>
-    <a href="<?= __ROOT_URL__ ?>/register">Register</a>
-<?php endif; ?>
+ <header>
+    <a href="<?= __ROOT_URL__ ?>/home">
+        <img  src="./assets/icons/logo.svg"/>
+    </a>
+    <div>
+        <button id="dialogHeader">
+            <img src="./assets/icons/burger.svg" />
+        <?php if($data['user_logged_in']): ?>
+            <span><?= ($data['user_logged_in'])->getFirstname() ?></span>
+        <?php else: ?>
+            <img src="./assets/icons/userIcon.svg" />
+        <?php endif; ?>
+        </button>
+        <div id="headerModal" class="hideModal">
+        <?php if($data['user_logged_in']): ?>
+            <a href="<?= __ROOT_URL__ ?>/logout">Logout</a>
+        <?php else: ?>
+            <a href="<?= __ROOT_URL__ ?>/login">Connexion</a>
+            <a href="<?= __ROOT_URL__ ?>/register">Inscription</a>
+        <?php endif; ?>
+        </div>
+
+    </div>
+ </header>
