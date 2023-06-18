d61492bfcdda5a8393ab232433221c9ef3d7faf0
Louisan TCHITOULA <t.louisan@outlook.fr>
2023-06-18 15:46:45 +0200

    chore: more accurate margins

--- src/view/template/header.php
+++ src/view/template/header.php
@@ -13,6 +13,9 @@
         </button>
         <div id="headerModal" class="hideModal">
         <?php if($data['user_logged_in']): ?>
+            <!-- besoin de différencier admin et client pour emmener au bon dashboard -->
+            <a href="<?= __ROOT_URL__ ?>/dashboard">Dashboard</a>
+            <a href="<?= __ROOT_URL__ ?>/settings">Réglages</a>
             <a href="<?= __ROOT_URL__ ?>/logout">Logout</a>
         <?php else: ?>
             <a href="<?= __ROOT_URL__ ?>/login">Connexion</a>
