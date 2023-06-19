7bc17c8191bfb5e96b08b42c5740a305434356d3
Alessandro Garau <alessandrogarau.pro@gmail.com>
2023-06-19 10:45:34 +0200

    fix: parametre url redirection

--- src/view/content/dashboardVotreEspace.php
+++ src/view/content/dashboardVotreEspace.php
@@ -2,5 +2,4 @@
 <div class="container">
 </div>
 
-<a href="<?= __ROOT_URL__ ?>/dashboard/parametre">Edit parametre</a>
 <script src="<?= __ROOT_URL__ ?>/scripts/dashboardVotreEspace.js" type="module"></script>
