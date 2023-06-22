32ffe0bdbe195fbac40c4cee5bd3b3b681cb2cc0
Louisan TCHITOULA <t.louisan@outlook.fr>
2023-06-22 10:42:27 +0200

    fix: layout dashboard fixed footer position & section spacing

--- src/view/content/dashboard.php
+++ src/view/content/dashboard.php
@@ -1,5 +1,9 @@
-<h1>Votre espace</h1>
-<div class="container">
-</div>
+<main>
+    <section>
+        <h1>Votre espace</h1>
+        <div class="container">
+        </div>
+    </section>
+</main>
 
 <script src="<?= __ROOT_URL__ ?>/scripts/dashboardVotreEspace.js" type="module"></script>
