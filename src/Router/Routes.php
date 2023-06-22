c969a0792fa9a84320b872500f839f8ad445f8df
Julien Heitz <contact@heitzjulien.com>
2023-06-23 00:42:07 +0200

    fix: routes

--- src/Router/Routes.php
+++ src/Router/Routes.php
@@ -18,22 +18,22 @@ class Routes {
     public function __construct(){
         $this->addRoute(
             (new Route("login", AuthController::class, "login"))
-            ->setTitle("Login")
+            ->setTitle("Connexion")
         );
         
         $this->addRoute(
             (new Route("login", AuthController::class, "login"))
-            ->setMethod("POST")->setTitle("Login")
+            ->setMethod("POST")->setTitle("Connexion")
         );
 
         $this->addRoute(
             (new Route("register", AuthController::class, "register"))
-            ->setTitle("Register")
+            ->setTitle("Inscription")
         );
         
         $this->addRoute(
             (new Route("register", AuthController::class, "register"))
-            ->setMethod("POST")->setTitle("Register")
+            ->setMethod("POST")->setTitle("Inscription")
         );
 
         $this->addRoute(
@@ -81,17 +81,17 @@ class Routes {
 
         $this->addRoute(
             (new Route("search", PublicController::class, "search"))
-            ->setTitle("Search")
+            ->setTitle("Recherche")
         );
 
         $this->addRoute(
             (new Route("search", PublicController::class, "search"))
-            ->setMethod("POST")->setTitle("Search")
+            ->setMethod("POST")->setTitle("Recherche")
         );
 
         $this->addRoute(
             (new Route("apartment", PublicController::class, "apartment"))
-            ->setTitle("Apartment")
+            ->setTitle("Appartement")
         );
 
         $this->addRoute(
