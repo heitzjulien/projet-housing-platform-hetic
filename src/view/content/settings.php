c542817a12e92ef10405d4d6d7d8892af80362db
mgfahem <mgfahem@gmail.com>
2023-06-24 19:54:09 +0200

    update: settings page

--- src/view/content/settings.php
+++ src/view/content/settings.php
@@ -11,49 +11,56 @@
     <?php endif; ?>
 </div>
 
-<h2>Settings</h2>
+<h1>Paramètres</h1>
 
-<h3>User settings</h3>
-<form method="POST">
-    <label for="firstname">Firstname :</label>
-    <input id="firstname" name="firstname" type="text" placeholder="Firstname" value="<?= $data["user_logged_in"]->getFirstname() ?>">
-    <label for="lastname">Lastname :</label>
-    <input id="lastname" name="lastname" type="text" placeholder="Lastname" value="<?= $data["user_logged_in"]->getLastname() ?>">
-    <label for="mail">Mail :</label>
-    <input id="mail" name="mail" type="text" placeholder="exemple@mail.com" value="<?= $data["user_logged_in"]->getMail() ?>">
-    <label for="birthdate">Birthdate :</label>
-    <input id="birthdate" name="birthdate" type="date" value="<?= $data["user_logged_in"]->getBirthdate() ?>">
-    <input type="submit" value="Update">
-</form>
+<section>
+    <div class="user">
+        <h3>Paramètres de l'utilisateur</h3>
+        <form method="POST">
+            <label for="firstname">Prénom </label>
+            <input id="firstname" name="firstname" type="text" placeholder="Firstname" value="<?= $data["user_logged_in"]->getFirstname() ?>">
+            <label for="lastname">Nom </label>
+            <input id="lastname" name="lastname" type="text" placeholder="Lastname" value="<?= $data["user_logged_in"]->getLastname() ?>">
+            <label for="mail">Mail </label>
+            <input id="mail" name="mail" type="text" placeholder="exemple@mail.com" value="<?= $data["user_logged_in"]->getMail() ?>">
+            <label for="birthdate">Date de naissance </label>
+            <input id="birthdate" name="birthdate" type="date" value="<?= $data["user_logged_in"]->getBirthdate() ?>">
+            <input type="submit" value="Update" class="btn-submit">
+        </form>
+    </div>
+    
+    <div class="user">
+        <h3>Validation du compte</h3>
+        <p>Votre compte n'est pas <?= $data["user_logged_in"]->getAccountStatus() ?></p>
+        <?php if($data["user_logged_in"]->getAccountStatus() == 'waiting'): ?>
+            <p>Veuillez valider votre email. <a href="<?= __ROOT_URL__ ?>/dashboard/parametre?action=sendmail">Renvoyer la confirmation</a></p>
+        <?php elseif($data["user_logged_in"]->getAccountStatus() == 'valid'): ?>
+            <p><a href="<?= __ROOT_URL__ ?>/dashboard/parametre?action=disable" id="btn-account">Désactiver le compte</a></p>
+        <?php elseif($data["user_logged_in"]->getAccountStatus() == 'disable'): ?>
+            <p><a href="<?= __ROOT_URL__ ?>/dashboard/parametre?action=valid" id="btn-account">Réactiver le compte</a></p>
+        <?php endif; ?>
+    </div>
+    
+    <div class="user">
+        <h3>Paramètres de sécurité</h3>
+        <form method="POST">
+            <label for="currentPsd">Mot de passe actuel</label>
+            <input id="currentPsd" name="currentPsd" type="password" placeholder="Mot de passe">
+            <label for="newPsd">Nouveau mot de passe</label>
+            <input id="newPsd" name="newPsd" type="password" placeholder="Nouveau le mot de passe">
+            <label for="confNewPsd">Confirmer le nouveau mot de passe</label>
+            <input id="confNewPsd" name="confNewPsd" type="password" placeholder="Confirmer le mot de passe">
+            <input type="submit" value="Update" class="btn-submit">
+        </form>
+    </div>
+    
+    <div class="user">
+        <h3>Déconnexion et suppression du compte</h3>
+        <div class="user-choice">
+            <a href="<?= __ROOT_URL__ ?>/logout?device=current">Déconnexion</a>
+            <a href="<?= __ROOT_URL__ ?>/logout?device=all">Déconnexion de tous les appareils</a>
+            <a href="<?= __ROOT_URL__ ?>/delete" id="btn-delete">Supprimer le compte</a>
+        </div>
+    </div>
 
-<br>
-
-<h3>Account validation</h3>
-<p>Your accoun't is <?= $data["user_logged_in"]->getAccountStatus() ?></p>
-<?php if($data["user_logged_in"]->getAccountStatus() == 'waiting'): ?>
-    <p>Please validate your email. <a href="<?= __ROOT_URL__ ?>/dashboard/parametre?action=sendmail">Resend the confirmation</a></p>
-<?php elseif($data["user_logged_in"]->getAccountStatus() == 'valid'): ?>
-    <p><a href="<?= __ROOT_URL__ ?>/dashboard/parametre?action=disable">Disable account</a></p>
-<?php elseif($data["user_logged_in"]->getAccountStatus() == 'disable'): ?>
-    <p><a href="<?= __ROOT_URL__ ?>/dashboard/parametre?action=valid">Reactivate account</a></p>
-<?php endif; ?>
-
-<br>
-
-<h3>Security settings</h3>
-<form method="POST">
-    <label for="currentPsd">Current Password :</label>
-    <input id="currentPsd" name="currentPsd" type="password" placeholder="Password">
-    <label for="newPsd">New Password :</label>
-    <input id="newPsd" name="newPsd" type="password" placeholder="Confirm password">
-    <label for="confNewPsd">Confirm New Password :</label>
-    <input id="confNewPsd" name="confNewPsd" type="password" placeholder="Confirm password">
-    <input type="submit" value="Update">
-</form>
-
-<br>
-
-<h3>Logout and Delete Account</h3>
-<a href="<?= __ROOT_URL__ ?>/logout?device=current">Logout</a>
-<a href="<?= __ROOT_URL__ ?>/logout?device=all">Logout All Device</a>
-<a href="<?= __ROOT_URL__ ?>/delete">Delete Account</a>
+</section>
